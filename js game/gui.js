0e7c8c96d9bd067e4fea94514b48bc68ea7d0c35
TeeNik <teenik3315@gmail.com>
2017-02-18 14:56:54 +0300

    Add files via upload

---
+++ js game/gui.js
@@ -0,0 +1,17 @@
+var healthBar = []
+/*var hFull = game.newImageObject({
+	"assets/hFull.png"
+});
+var hHalf = pjs.tiles.newImage("assets/hHalf.png");
+var hEmp = pjs.tiles.newImage("assets/hEmp.png");
+
+healthBar.push(hFull);
+healthBar.push(hFull);
+healthBar.push(hFull);
+
+OOP.forArr(healthBar, function(el){
+	el.draw();
+});*/
+
+var j = pjs.GUI.newImage("assets/hHalf.png");
+j.setPositionC(100,100);
